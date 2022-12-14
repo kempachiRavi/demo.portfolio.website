a9debffaf666c92d17fd4265921ac766e7e4c029
RAVI PRATAP SINGH <ravipratapsingh3628@gmail.com>
2022-12-14 20:54:14 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    let $btns=$('.button');
+
+    $btns.click(function(e){
+
+        $('.button').removeClass(active);
+        e.target.classList.add(active);
+
+        let selector = $(e.target).attr(data-filter);
+
+        $('.project-area.grid').isotope({
+            filter:selector
+        });
+
+        return false;
+
+    })
+
+    //owl carousel
+    $('.owl-carousel' ).owlCarousel({
+        loop:true,
+        autoplay:true,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            544:{
+                items:2
+            }
+        }
+
+    })
+
+    // //sticky navigation menu
+    // let nav_offset_top=$('.header_area').height()+50;
+    // function navbarFixed(){
+    //     if($('.header_area').length){
+    //         $(window).scroll(function(){
+    //             let scroll=$(window).scrollTop();
+    //             if(scroll>=nav_offset_top){
+    //                 $('.header_area.main-menu').addClass('navbar_fixed');
+    //             }else{
+    //                     $('.header_area.main-menu').addClass('navbar_fixed');
+    //                 }
+                
+    //         })
+    //     }
+    // }
+    //     navbarFixed();
+});
+
